992ea71c0ea9b7fa5fdea56aac0f279fe138a131
ayano hirayama <a.hirayama@newdaysys.co.jp>
2014-06-06 18:55:17 +0900

    use refarence

---
+++ 2ex4_2.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use strict;
+use Data::Dumper;
+
+sub check_required_item {
+    my $who = shift;
+    my $items = shift;
+
+    my @required = qw (preserver sunscreen water_bottle jacket);
+    for my $req_item(@required){
+	unless(grep $req_item eq $_, @$items){
+	    print "$who is missing $req_item.","\n";
+	}
+    }
+}
+
+sub check_item_for_all{
+    for my $who (keys %{$_[0]}) {
+	check_required_item($who,$all{$who});
+    }
+}
+
+my @gilligan = qw (red_shirt hat lucky_socks water_bottle);
+my @skipper = qw (blue_shirt hat jacket preserver sunscreen);
+my @professor = qw (sunscreen water_bottle slide_rule batteries radio);
+my %all = (
+    Gilligan => \@gilligan,
+    Skipper => \@skipper,
+    Professor => \@professor,
+    );
+
+check_item_for_all(\%all);
+
