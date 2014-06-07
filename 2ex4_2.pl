5c9d9fa62b2a36ba380da42f3e55649b7d03e5d8
平山文乃 <@jun.(none)>
2014-06-07 10:05:48 +0900

    皆のアイテムが取得できていなかった問題を修正

--- 2ex4_2.pl
+++ 2ex4_2.pl
@@ -5,18 +5,25 @@ use Data::Dumper;
 sub check_required_item {
     my $who = shift;
     my $items = shift;
-
+    my @miss;
     my @required = qw (preserver sunscreen water_bottle jacket);
     for my $req_item(@required){
 	unless(grep $req_item eq $_, @$items){
 	    print "$who is missing $req_item.","\n";
+	    push @miss, $req_item;
 	}
+    }
+    
+    if (@miss) {
+	push @$items, @miss;
+	print "add missing items.", "\n";
+	print "@$items", "\n";
     }
 }
 
 sub check_item_for_all{
     for my $who (keys %{$_[0]}) {
-	check_required_item($who,$all{$who});
+	check_required_item($who,$_[0]->{$who});
     }
 }
 
