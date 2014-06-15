38332b7d3b84b97887ae4bf9da3fc7e0064cb117
ayano hirayama <a.hirayama@newdaysys.co.jp>
2014-06-15 13:23:57 +0900

    CPANを使ってモジュールをインストール

---
+++ alpaca-ex3-2.pl
@@ -0,0 +1,10 @@
+#!usr/bin/perl
+use strict;
+use Business::ISBN;
+use Data::Dumper;
+
+my $isbn10 = Business::ISBN->new('4873113059');
+#print Dumper \$isbn10;
+
+print "国別コード(group_code) : ".$isbn10->group_code."\n";
+print "出版社コード(publisher_code) : ".$isbn10->publisher_code."\n";
