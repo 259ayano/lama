996fafe6e22cc71c1283add9086a911c873fb3d1
HIRAYAMA Ayano <a.hirayama@newdaysys.co.jp>
2016-02-16 18:06:33 +0900

    mmm2

--- Weather/lib/Weather/Web/Controller/Tornado.pm
+++ Weather/lib/Weather/Web/Controller/Tornado.pm
@@ -10,8 +10,15 @@ BEGIN { extends 'Catalyst::Controller'; }
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
     my $csv = Weather::CSV->connect;
-    my @tornado = $csv->tornado();
-    warn Dumper \@tornado;
+	my $params = expand_hash($c->req->params);
+	
+	my $where = { 'type' => $params->{search}{hint} };
+   my @tornado = $csv->tornado($where);
+	my @title = qw/type date position fscale damage1 damage2 dead
+                   hurt1 alldestroy halfdestroy detail/;
+
+	$c->stash->{th} = \@title;
+	$c->stash->{list} = \@tornado;
     $c->stash->{template} = 'tornado.tt';
 }
 
